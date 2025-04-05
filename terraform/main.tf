d39e6c4433fefc1de58d3661a9766e3173cfacaf
mishra-rd015 <154001355+mishra-rd015@users.noreply.github.com>
2025-04-05 17:22:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,41 @@
 provider "azurerm" {
   features {}
-  subscription_id = "4947feb5-b5f6-4284-acce-df1b262aedb0"
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
-resource "azurerm_resource_group" "this" {
-  name     = "rg-jenkins"
-  location = "East US"  # Replace with your desired location or var.location
+resource "azurerm_resource_group" "rg_jenkins" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-resource "azurerm_service_plan" "this" {
-  name                = "appserviceplan-jenkins"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  os_type             = "Linux"  # Required argument, specify "Linux" or "Windows"
-  
-  sku_name            = "B1"     # Pricing tier (e.g., B1, S1, P1V2)
-  sku_tier            = "Basic"  # SKU tier (Basic, Standard, Premium, etc.)
+resource "azurerm_service_plan" "appserviceplan_jenkins" {
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.rg_jenkins.location
+  resource_group_name = azurerm_resource_group.rg_jenkins.name
+  os_type             = "Linux"
 
-  # Optional: Configure the number of workers (for scaling purposes)
-  # reserved            = true    # If you are using Linux, set this to true
+  sku {
+    tier = var.sku_tier
+    size = var.sku_size
+  }
 }
 
-resource "azurerm_linux_web_app" "this" {
-  name                = "webapijenkinshardik827813"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
-  service_plan_id     = azurerm_service_plan.this.id
+resource "azurerm_linux_web_app" "jenkins_app" {
+  name                = var.web_app_name
+  location            = azurerm_resource_group.rg_jenkins.location
+  resource_group_name = azurerm_resource_group.rg_jenkins.name
+  service_plan_id     = azurerm_service_plan.appserviceplan_jenkins.id
 
   site_config {
-    always_on = true  # Keeps the app always running
+    always_on = true
   }
 
   app_settings = {
-    "WEBSITE_NODE_DEFAULT_VERSION" = "14.15"  # Node.js version
+    "WEBSITE_NODE_DEFAULT_VERSION" = "14.15"
   }
 
   tags = {
