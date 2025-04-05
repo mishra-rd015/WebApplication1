6cd4512aba9ab861876269450180d6f6fd7cfef0
mishra-rd015 <154001355+mishra-rd015@users.noreply.github.com>
2025-04-05 15:45:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = "4947feb5-b5f6-4284-acce-df1b262aedb0"
+  client_id       = "16a790e6-0069-4397-af16-1c0f0ca9a9bf"
+  client_secret   = "cXo8Q~wv3nv3g5WUCddUyxIoA0Hvh64OOVMabaJ6"
+  tenant_id       = "2ab9811e-8172-4cd1-bf22-858c28b732da"
 }
 
 resource "azurerm_resource_group" "this" {
