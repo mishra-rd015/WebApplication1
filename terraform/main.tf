ed0432865af25504eb4ccdfb915316f8df539c62
mishra-rd015 <154001355+mishra-rd015@users.noreply.github.com>
2025-04-05 17:26:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.26.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-jenkins"
+    storage_account_name = "flasktfstorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_version = ">= 1.1.0"
+}
+
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  subscription_id = "4947feb5-b5f6-4284-acce-df1b262aedb0"
+  client_id       = "16a790e6-0069-4397-af16-1c0f0ca9a9bf"
+  client_secret   = "cXo8Q~wv3nv3g5WUCddUyxIoA0Hvh64OOVMabaJ6"
+  tenant_id       = "2ab9811e-8172-4cd1-bf22-858c28b732da"
 }
 
 resource "azurerm_resource_group" "rg_jenkins" {
@@ -13,18 +31,14 @@ resource "azurerm_resource_group" "rg_jenkins" {
 }
 
 resource "azurerm_service_plan" "appserviceplan_jenkins" {
-  name                = var.app_service_plan_name
+  name                = var.service_plan_name
   location            = azurerm_resource_group.rg_jenkins.location
   resource_group_name = azurerm_resource_group.rg_jenkins.name
   os_type             = "Linux"
-
-  sku {
-    tier = var.sku_tier
-    size = var.sku_size
-  }
+  sku_name            = "B1"
 }
 
-resource "azurerm_linux_web_app" "jenkins_app" {
+resource "azurerm_linux_web_app" "jenkins_app_12345" {
   name                = var.web_app_name
   location            = azurerm_resource_group.rg_jenkins.location
   resource_group_name = azurerm_resource_group.rg_jenkins.name
