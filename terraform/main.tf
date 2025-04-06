9bce24f515e1cdbce1279081f3a215dc3050d86a
mishra-rd015 <154001355+mishra-rd015@users.noreply.github.com>
2025-04-06 22:58:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,58 +1,29 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.26.0"
-    }
-  }
-
-  backend "azurerm" {
-    resource_group_name  = "rg-jenkins"
-    storage_account_name = "flasktfstorage"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-  }
-
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
-
   subscription_id = "4947feb5-b5f6-4284-acce-df1b262aedb0"
-  client_id       = "16a790e6-0069-4397-af16-1c0f0ca9a9bf"
-  client_secret   = "cXo8Q~wv3nv3g5WUCddUyxIoA0Hvh64OOVMabaJ6"
-  tenant_id       = "2ab9811e-8172-4cd1-bf22-858c28b732da"
 }
 
-resource "azurerm_resource_group" "rg_jenkins" {
-  name     = var.resource_group_name
+resource "azurerm_resource_group" "this" {
+  name     = "rg-jenkins"
   location = var.location
 }
 
-resource "azurerm_service_plan" "appserviceplan_jenkins" {
-  name                = var.service_plan_name
-  location            = azurerm_resource_group.rg_jenkins.location
-  resource_group_name = azurerm_resource_group.rg_jenkins.name
+resource "azurerm_service_plan" "this" {
+  name                = "appserviceplan-jenkins"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   os_type             = "Linux"
-  sku_name            = "B1"
+
+  sku_name = "B1"
 }
 
-resource "azurerm_linux_web_app" "jenkins_app_12345" {
-  name                = var.web_app_name
-  location            = azurerm_resource_group.rg_jenkins.location
-  resource_group_name = azurerm_resource_group.rg_jenkins.name
-  service_plan_id     = azurerm_service_plan.appserviceplan_jenkins.id
+resource "azurerm_linux_web_app" "this" {
+  name                = "webapijenkinsrudram015"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  service_plan_id     = azurerm_service_plan.this.id  # Updated attribute
 
   site_config {
     always_on = true
-  }
-
-  app_settings = {
-    "WEBSITE_NODE_DEFAULT_VERSION" = "14.15"
-  }
-
-  tags = {
-    environment = "production"
   }
 }
